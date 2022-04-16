f25cf7a4cdeea71f7580ff232b51636bc17b981b
theophiIe <theomolinatti@gmail.com>
2022-04-16 12:02:07 +0200

    [create][feature] : connexion à la base de donnée côté client et serveur

---
+++ config/database_client.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const { Client } = require('pg')
+
+const client = new Client({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+})
+client.connect()
+
+module.exports = client
