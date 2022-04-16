9f82ae0071119046e2a43cd31dd5765d081eccf2
theophiIe <theomolinatti@gmail.com>
2022-04-16 12:04:02 +0200

    [create][feature] : création de la route vers /home et appel des lib

---
+++ server.js
@@ -0,0 +1,36 @@
+// Permet l'utilisation du fichier de configuration .env
+require('dotenv').config()
+
+// Permet d'utiliser les lib
+const express = require("express")
+
+// Créer une instance d'express
+let app = express()
+
+// Permet de setup EJS
+app.set('view engine', 'ejs')
+
+// Permet l'utilisation des fichiers dans public et dans script
+app.use('/public', express.static('public'))
+app.use('/script', express.static('script'))
+
+// Permet l'utilisation des fichiers dans public et dans script
+app.use('/public', express.static('public'))
+
+// Les routes
+// Route vers la page Homepage
+app.get('/', (req, res) => {
+    res.redirect('/home')
+})
+
+// Route vers la page Homepage
+app.get('/home', (req, res) => {
+    res.render('pages/home')
+})
+
+// Permet de rediger toutes les autres url vers la page Home
+app.get('/*', (req, res) => {
+    res.redirect('/home')
+})
+
+app.listen(8080)
