7e68788fea7cf948dc2f71a08f247e9e43740ae3
theophiIe <theomolinatti@gmail.com>
2022-04-20 18:26:00 +0200

    [feature] : ajout de la route vers chart avec les paramètres pour afficher un graphique

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 // Permet d'utiliser les lib
 const express = require("express");
 
+// Include des fichiers du dossier models
+const Auteur = require("./models/auteur");
+const { EcritPar, Count_auteurs } = require("./models/ecrit_par");
+const { Parle_de, Count_personnalite } = require("./models/parle_de");
+
 // Créer une instance d'express
-let app = express();
+const app = express();
 app.disable("x-powered-by");
 
 // Permet de setup EJS
@@ -19,16 +24,36 @@ app.use('/script', express.static('script'));
 // Route vers la page Homepage
 app.get('/', (req, res) => {
     res.redirect('/home');
-})
+});
 
 // Route vers la page Homepage
-app.get('/home', (req, res) => {
+app.get('/home', async (req, res) => {
     res.render('pages/home');
-})
+});
+
+app.get('/chart', async (req, res) => {
+    const data_auteurs = await Count_auteurs.count_nombre_auteurs();
+
+    let data = {};
+    let other_value = 0;
+
+    for (let dataAuteursKey in data_auteurs) {
+        if (dataAuteursKey <= 4) {
+            data[data_auteurs[dataAuteursKey].nom] = parseInt(data_auteurs[dataAuteursKey].nombre);
+        }
+        else {
+            other_value += parseInt(data_auteurs[dataAuteursKey].nombre);
+        }
+    }
+
+    data['other'] = other_value;
+
+    res.render('pages/chart', {data: data});
+});
 
 // Permet de rediger toutes les autres url vers la page Home
 app.get('/*', (req, res) => {
     res.redirect('/home');
-})
+});
 
 app.listen(8080);
