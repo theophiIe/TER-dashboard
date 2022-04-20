a8972d481e82bf400fdc750b90428801823b36ae
theophiIe <theomolinatti@gmail.com>
2022-04-20 15:26:20 +0200

    [feature] : ajout de nouvelle requête SQL
    Création de la classe `Count_auteurs` et des méthodes :
     - count_nombre_auteurs
     - count_nombre_relecteur
     - count_nombre_secretariat

--- models/ecrit_par.js
+++ models/ecrit_par.js
@@ -5,22 +5,26 @@ class Ecrit_par {
         this.row = row;
     }
 
-    get URL() {
-        return this.row.URL;
+    get url() {
+        return this.row.url;
     }
 
     get nom() {
         return this.row.nom;
     }
 
-    get role() {
-        return this.row.Roles;
+    get roles() {
+        return this.row.roles;
     }
 
-    static find(URL, nom, role) {
-        const sql = 'SELECT * FROM t_ecritpar WHERE URL = $1 AND nom = $2 AND Roles = $3';
+    get nombre() {
+        return this.row.nombre;
+    }
 
-        return client.query(sql, [URL, nom, role])
+    static find(url, nom, role) {
+        const sql = 'SELECT * FROM t_ecritpar WHERE url = $1 AND nom = $2 AND roles = $3';
+
+        return client.query(sql, [url, nom, role])
             .then(result => new Ecrit_par(result.rows[0]))
             .catch(e => console.error(e.stack));
     }
@@ -34,4 +38,42 @@ class Ecrit_par {
     }
 }
 
-module.exports = Ecrit_par;
+class Count_auteurs {
+    constructor(row) {
+        this.row = row;
+    }
+
+    get nom() {
+        return this.row.nom;
+    }
+
+    get nombre() {
+        return this.row.nombre;
+    }
+
+    static count_nombre_auteurs() {
+        const sql = 'SELECT nom, COUNT(*) as nombre FROM t_ecritpar WHERE roles = $1 GROUP BY nom ORDER BY nombre desc'
+
+        return client.query(sql, ['Auteur'])
+            .then(result => result.rows.map(res => new Count_auteurs(res)))
+            .catch(e => console.error(e.stack));
+    }
+
+    static count_nombre_relecteur() {
+        const sql = 'SELECT nom, COUNT(*) as nombre FROM t_ecritpar WHERE roles = $1 GROUP BY nom ORDER BY nombre desc'
+
+        return client.query(sql, ['Relecteur'])
+            .then(result => result.rows.map(res => new Count_auteurs(res)))
+            .catch(e => console.error(e.stack));
+    }
+
+    static count_nombre_secretariat() {
+        const sql = 'SELECT nom, COUNT(*) as nombre FROM t_ecritpar WHERE roles = $1 GROUP BY nom ORDER BY nombre desc'
+
+        return client.query(sql, ['Secretariat'])
+            .then(result => result.rows.map(res => new Count_auteurs(res)))
+            .catch(e => console.error(e.stack));
+    }
+}
+
+module.exports = { Ecrit_par,  Count_auteurs };
