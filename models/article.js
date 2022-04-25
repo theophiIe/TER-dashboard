edb15d8016e45ec4a7fb260e240cba0c97bff5a7
theophiIe <theomolinatti@gmail.com>
2022-04-25 15:05:51 +0200

    [feature] : création de la class Article_mois et des méthodes nombre_date_creation_par_mois et nombre_date_modification_par_mois

--- models/article.js
+++ models/article.js
@@ -1,3 +1,4 @@
+/* eslint max-classes-per-file: ["error", 2] */
 const client = require('../config/database_client')
 const moment = require('../config/moment')
 
@@ -51,4 +52,34 @@ class Article {
     }
 }
 
-module.exports = Article;
+class Article_mois {
+    constructor(row) {
+        this.row = row;
+    }
+
+    get date() {
+        return moment(this.row.date);
+    }
+
+    get nombre() {
+        return this.row.nombre;
+    }
+
+    static nombre_date_creation_par_mois() {
+        const sql = 'SELECT DATE_TRUNC(\'month\', a.date_creation) AS date, COUNT(*) AS nombre from t_article a WHERE a.date_creation IS NOT NULL GROUP BY DATE_TRUNC(\'month\', a.date_creation) ORDER BY DATE_TRUNC(\'month\', a.date_creation)'
+
+        return client.query(sql, [])
+            .then(result => result.rows.map(res => new Article_mois(res)))
+            .catch(e => console.error(e.stack));
+    }
+
+    static nombre_date_modification_par_mois() {
+        const sql = 'SELECT DATE_TRUNC(\'month\', a.date_modification) AS date, COUNT(*) AS nombre from t_article a WHERE a.date_modification IS NOT NULL GROUP BY DATE_TRUNC(\'month\', a.date_modification) ORDER BY DATE_TRUNC(\'month\', a.date_modification)'
+
+        return client.query(sql, [])
+            .then(result => result.rows.map(res => new Article_mois(res)))
+            .catch(e => console.error(e.stack));
+    }
+}
+
+module.exports = { Article, Article_mois };
