0a6a24b03d4dfbbe66a1ac34742c59eb87ecd5da
uvsq21805057 <matrixzeyphax@gmail.com>
2022-04-25 18:31:59 +0200

    [add] : Ajout du modèle pour calculer les data de la page informations

---
+++ models/nombre.js
@@ -0,0 +1,53 @@
+const client = require('../config/database_client')
+
+class Nombre {
+    constructor(row) {
+        this.row = row;
+    }
+
+    get nombre() {
+        return this.row.nombre;
+    }
+
+    static nombre_article() {
+        const sql = 'SELECT COUNT(*) AS nombre FROM t_article';
+
+        return client.query(sql, [])
+            .then(result => new Nombre(result.rows[0]))
+            .catch(e => console.error(e.stack));
+    }
+
+    static nombre_auteur() {
+        const sql = 'SELECT COUNT(*) AS nombre FROM t_auteur';
+
+        return client.query(sql, [])
+            .then(result => new Nombre(result.rows[0]))
+            .catch(e => console.error(e.stack));
+    }
+
+    static nombre_personnalite() {
+        const sql = 'SELECT COUNT(*) AS nombre FROM t_personnalite';
+
+        return client.query(sql, [])
+            .then(result => new Nombre(result.rows[0]))
+            .catch(e => console.error(e.stack));
+    }
+
+    static nombre_reference() {
+        const sql = 'SELECT COUNT(*) AS nombre FROM t_reference';
+
+        return client.query(sql, [])
+            .then(result => new Nombre(result.rows[0]))
+            .catch(e => console.error(e.stack));
+    }
+
+    static nombre_source() {
+        const sql = 'SELECT COUNT(*) AS nombre FROM t_source';
+
+        return client.query(sql, [])
+            .then(result => new Nombre(result.rows[0]))
+            .catch(e => console.error(e.stack));
+    }
+}
+
+module.exports = Nombre;
